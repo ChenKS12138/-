81e0b25fd98ce18e336218a3ad974b8e4bf7f428
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2018-11-09 17:02:13 +0800

    scroll-init

--- js/scroll.js
+++ js/scroll.js
@@ -3,22 +3,27 @@ function change_color(height){
     if(height<500){
         document.getElementById('tool-bar').style.backgroundColor='teal';
         document.getElementById('tool-bar').style.color='snow';
+        barColor(0);
     }
     else if(height<1100){
         document.getElementById('tool-bar').style.backgroundColor='cadetblue';
         document.getElementById('tool-bar').style.color='snow';
+        barColor(1);
     }
     else if(height<1800){
         document.getElementById('tool-bar').style.backgroundColor='tomato';
         document.getElementById('tool-bar').style.color='snow';
+        barColor(2);
     }
     else if(height<2700){
         document.getElementById('tool-bar').style.backgroundColor='wheat';
         document.getElementById('tool-bar').style.color='slategray';
+        barColor(3);
     }
     else{
         document.getElementById('tool-bar').style.backgroundColor='snow';
         document.getElementById('tool-bar').style.color='slategray';
+        barColor(4);
     }
 }
 
@@ -31,7 +36,18 @@ function getScrollTop(){
 	} 
 	return scrollTop; 
 } 
+function barColor(id){
+    var i;
+    var bar=document.getElementsByClassName("color-change");
+    for(i=0;i<5;i++){
+        bar[i].style.color='';
+    }
+    bar[id].style.color='black';
+}
+
+
+
 document.onscroll = function(){
-    console.log(getScrollTop());
-    change_color(getScrollTop());
+    var height=getScrollTop();
+    change_color(height);
 }
