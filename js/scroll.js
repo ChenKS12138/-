90c8bf15ff78730ad4b6ecccef6c7f6978040c09
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2018-11-09 15:40:28 +0800

    'init'

--- js/scroll.js
+++ js/scroll.js
@@ -2,18 +2,23 @@
 function change_color(height){
     if(height<500){
         document.getElementById('tool-bar').style.backgroundColor='teal';
+        document.getElementById('tool-bar').style.color='snow';
     }
     else if(height<1100){
         document.getElementById('tool-bar').style.backgroundColor='cadetblue';
+        document.getElementById('tool-bar').style.color='snow';
     }
     else if(height<1800){
         document.getElementById('tool-bar').style.backgroundColor='tomato';
+        document.getElementById('tool-bar').style.color='snow';
     }
     else if(height<2700){
         document.getElementById('tool-bar').style.backgroundColor='wheat';
+        document.getElementById('tool-bar').style.color='slategray';
     }
     else{
         document.getElementById('tool-bar').style.backgroundColor='snow';
+        document.getElementById('tool-bar').style.color='slategray';
     }
 }
 
