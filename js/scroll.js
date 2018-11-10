7bcb56964dd1bd3582e2e3427b0966b47604c62c
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2018-11-10 11:29:08 +0800

    'MD'

--- js/scroll.js
+++ js/scroll.js
@@ -1,40 +1,58 @@
 function change_color(height){
     var bar=document.getElementById('tool-bar');
+    var nvBar=document.getElementById('navigation-bar');
     var menu=document.getElementById('menu');
-    var h1=document.getElementsByClassName('tip1')[0].offsetTop;
-    var h2=document.getElementsByClassName('tip2')[0].offsetTop;
-    var h3=document.getElementsByClassName('tip3')[0].offsetTop;
-    var h4=document.getElementsByClassName('tip4')[0].offsetTop;
+    var barTitle=document.getElementById('bar-title');
 
+    var h1=document.getElementsByClassName('tip1')[0].offsetTop-50;
+    var h2=document.getElementsByClassName('tip2')[0].offsetTop-50;
+    var h3=document.getElementsByClassName('tip3')[0].offsetTop-50;
+    var h4=document.getElementsByClassName('tip4')[0].offsetTop-50;
+    if(height!=0){
+        nvBar.style.fontSize='1.2rem';
+        nvBar.style.height='3rem';
+        bar.style.boxShadow='';
+    }
+    else{
+        nvBar.style.fontSize='1.5rem';
+        nvBar.style.height='4rem';
+        bar.style.boxShadow='none';
+    }
     if(height<h1){
         bar.style.backgroundColor='teal';
         bar.style.color='snow';
         barColor(0);
         menu.setAttribute('src','img/menu_white.png');
+        barTitle.style.display='none';
+        
     }
     else if(height<h2){
         bar.style.backgroundColor='cadetblue';
         bar.style.color='snow';
         barColor(1);
         menu.setAttribute('src','img/menu_white.png');
+        barTitle.style.display='flex';
     }
     else if(height<h3){
         bar.style.backgroundColor='tomato';
         bar.style.color='snow';
         barColor(2);
         menu.setAttribute('src','img/menu_white.png');
+        barTitle.style.display='flex';
     }
     else if(height<h4){
         bar.style.backgroundColor='wheat';
         bar.style.color='slategray';
         barColor(3);
         menu.setAttribute('src','img/menu_grey.png');
+        barTitle.style.display='flex';
     }
     else{
         bar.style.backgroundColor='snow';
         bar.style.color='slategray';
         barColor(4);
         menu.setAttribute('src','img/menu_grey.png');
+        barTitle.style.display='flex';
     }
 }
 
