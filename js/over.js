b5eda1b6a143151458364ce881cb1856cf2c72b7
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2018-11-11 20:33:02 +0800

    'init'

---
+++ js/over.js
@@ -0,0 +1,10 @@
+
+var icons=document.getElementsByClassName("icon");
+var icon_black=["img/shouye_black.png","img/qita_black.png","img/lishi_black.png","img/ganxiang_black.png","img/jingdian_black.png","img/meishi_black.png"];
+var icon_white=["img/shouye_white.png","img/qita_white.png","img/lishi_white.png","img/ganxiang_white.png","img/jingdian_white.png","img/meishi_white.png"];
+function over(i){
+    icons[i].setAttribute("src",icon_black[i]);
+}
+function out(i){
+    icons[i].setAttribute("src",icon_white[i]);
+}
